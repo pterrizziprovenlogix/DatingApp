39f566de4bdf296c3004c1fd50ede7967f9835c1
Pterrizzi@hpone.com <Pterrizzi@hpone.com>
2019-07-08 14:20:02 -0400

    End of section 7

--- DatingAppNg/src/app/nav/nav.component.ts
+++ DatingAppNg/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -15,7 +16,7 @@ export class NavComponent implements OnInit {
     'but wait! A third!'
   ];
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -26,6 +27,8 @@ export class NavComponent implements OnInit {
     },
     error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -36,6 +39,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
   }
 
   onHidden(): void {
